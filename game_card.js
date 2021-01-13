0234d53f88bdb99367dfc150305a89d3dc0777b7
Valentin <51223584+MVV1983@users.noreply.github.com>
2021-01-13 17:46:53 +0700

    Update game_card.js

--- game_card.js
+++ game_card.js
@@ -24,9 +24,7 @@ for (let i = 0; i < 12; i++) {
 		//разброс карточек рандомно подве каждого зверька
 		for(let i = 0; i < 2; i++){
 			var bug = Math.round(Math.random()*4);
-		
-			let imageForCard = image[bug].substring(5,image[bug].length-2);
-			//alert(image[bug].substring(5,image[bug].length-2));
+		        let imageForCard = image[bug].substring(5,image[bug].length-2);
 			img.src = imageForCard;
 		}
 		//img.src = 'images/yellow_bug.jpg';
