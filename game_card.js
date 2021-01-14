1396d8acdab9e72fc5cef0203fcea61bd4efa6b9
Valentin <51223584+MVV1983@users.noreply.github.com>
2021-01-14 17:43:22 +0700

    Add files via upload
    Create Start menu and chandge color scheme

--- game_card.js
+++ game_card.js
@@ -1,14 +1,45 @@
-var image = new Array("url('images/ladybug.jpg')","url('images/bug2.jpg')","url('images/bug3.jpg')","url('images/yellow_bug.jpg')","url('images/muha.jpg')","url('images/bluebug.jpg')"); 
+let backGrnd = document.querySelector('body');
+backGrnd.style.backgroundColor='#43506C';
+
+let btnStart = document.createElement('button1');//запилим стартовую кнопку в начале игры
+btnStart.textContent="START";
+
+let aboutButton = document.createElement('button2');//запилим стартовую кнопку в начале игры
+aboutButton.textContent="ABOUT";
+
 let table = document.querySelector('.game_field');
+table.appendChild(btnStart);
+table.appendChild(aboutButton);
+
+let nav = document.querySelector('.navigation');
+let foot= document.querySelector('footer');
+nav.classList.toggle('hide');
+foot.classList.toggle('hide');
+
+btnStart.onclick = start;
+
+
+function start(){
+		alert('start game');
+		goGame();
+		table.removeChild(btnStart);
+		table.removeChild(aboutButton);
+		nav.classList.toggle('hide');
+		foot.classList.toggle('hide');
+}
+
+function goGame(){
+
+var image = new Array("url('images/ladybug.jpg')","url('images/bug2.jpg')","url('images/bug3.jpg')","url('images/yellow_bug.jpg')","url('images/muha.jpg')","url('images/bluebug.jpg')"); 
+//let table = document.querySelector('.game_field');
 var drawScore = document.querySelector('.count');
-drawScore.innerHTML="SCORE: "+0;
-drawScore.style.marginLeft = '400px';
+drawScore.innerHTML="Score: "+0;
+
+var doubleClick = "url('images/no.jpg')";
 var messageForPlayer = document.querySelector('.Message');
 
 
-
-
-for (let i = 0; i < 12; i++) {
+for (let i = 0; i < 16; i++) {
 	let tr = document.createElement('div');
 	tr.className = "game_row";
 	
@@ -24,7 +55,8 @@ for (let i = 0; i < 12; i++) {
 		//разброс карточек рандомно подве каждого зверька
 		for(let i = 0; i < 2; i++){
 			var bug = Math.round(Math.random()*5);
-			let imageForCard = image[bug].substring(5,image[bug].length-2);
+		    let imageForCard = image[bug].substring(5,image[bug].length-2);
+			//alert(image[bug].substring(5,image[bug].length-2));
 			img.src = imageForCard;
 		}
 		//img.src = 'images/yellow_bug.jpg';
@@ -45,7 +77,6 @@ var temp2;
 
 var card1;
 var card2;
-var isFlipped = false;
 let lockCard = false;
 
 cards.forEach(card => card.addEventListener('click', flipCard));
@@ -59,19 +90,20 @@ function flipCard() {
   
   clickCount++;
 
-if(clickCount == 1){
+
+	  if(clickCount == 1){
 	   card1= this;
+	   
 	   firstCard = this.querySelector('.front-face');
 	   card1.setAttribute('data',firstCard.src);
-	   isFlipped=true;
-	   firstCard.alt = isFlipped;
-	   temp1 = firstCard.alt;  
+	   firstCard.alt= "1";
+	   temp1 = firstCard.alt;
       }
-else if(this == card1){
-   alert('Эта карточка уже выбрана, выберите другую!');
-   this.removeEventListener('click', flipCard);
-}
-      else {//else if(clickCount == 2)
+	  else if(this == card1){
+		  this.removeEventListener('click', flipCard);
+		  messageForPlayer.innerHTML="Эта карточка уже выбрана. Выбирайте другую!";
+	  }
+      else{//else if(clickCount == 2)
 	   lockCard=true;
 	   card2= this;
 	   secondCard = this.querySelector('.front-face');
@@ -87,29 +119,41 @@ else if(this == card1){
                 var delay = ms => new Promise(resolve => { setTimeout(resolve, ms); });
                 await delay(1000);
                 // код функции для выполнения после приостановки на 1 секунду
-                matchesTwoCards(temp1,temp2,card1,card2);
+                matchesTwoCards(firstCard,temp2,card1,card2);
             }
 
 
 function matchesTwoCards(temp1,temp2,one,two){
-  
   var card1Atr = card1.getAttribute('data');
   var card2Atr = card2.getAttribute('data');
   
 	if(card1Atr == card2Atr){//if(temp1 == temp2){
+		
+		  //alert('первая карточка:'+one+'совпадает с Второй карточкой:'+two);
 		  countScore++;
 		  drawScore.innerHTML="SCORE: "+countScore;
+		  //let paragraph = document.querySelector('p');
+          //paragraph.textContent = countScore;
 		  clickCount=0;
 		  one.removeEventListener('click', flipCard);
 		  two.removeEventListener('click', flipCard);
 		  lockCard=false;
+		  //часть кода для открытия новых сверху слева персонажей
+		  let unit1 = document.querySelector('.unit1');
+		  let openUnit = temp1.getAttribute('src');
+		  //unit1.style.backgroundImage = "url('images/yellow_bug.jpg')";
+		  unit1.setAttribute("style","background-image: url(\""+openUnit+"\")");
+		  unit1.style.backgroundRepeat = "no-repeat";
+		  unit1.style.backgroundSize = "60px 100px";
 	  }
 	  else{
-		lockCard=false;
-		messageForPlayer.innerHTML="Карточки не совпадают!";
-		clickCount=0;
-		one.addEventListener('click', flipCard);
+		  lockCard=false;
+		  clickCount=0;
+		  messageForPlayer.innerHTML="Карточки не совпадают!";
+	      one.addEventListener('click', flipCard);
+		//alert(one + two);
 		one.classList.toggle('flip');
-		two.classList.toggle('flip');
+	    two.classList.toggle('flip');
 	  }
+  }
 }
